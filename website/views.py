434ffe0f6708f9f694a60bc636dab5739e458fdd
Rachael Harkavy <rachaelharkavy@Rachaels-MacBook-Pro.local>
2023-10-24 08:25:14 -0400

    cant get certificate to work

--- website/views.py
+++ website/views.py
@@ -12,7 +12,16 @@ import urllib.request
 from PIL import Image
 import matplotlib.pyplot as plt
 
+import urllib.request
+import ssl
+import requests
+import os 
+
+
+
+
 views = Blueprint('views', __name__)
+openai.api_key = "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"
 
 
 @views.route('/', methods=['GET', 'POST'])
@@ -53,9 +62,44 @@ def intro_flow_2():
 @views.route('/intro_flow_3', methods=['GET'])
 @login_required
 def intro_flow_3():
-    franchise = Franchise.query.first()  # Get the first (or appropriate) user response
-    print(franchise)
-    return render_template('intro_flow_3.html', user=current_user, franchise=franchise)
+    # Your existing code to retrieve the franchise data
+    franchise = Franchise.query.first()
+
+    ssl_context = ssl.create_default_context()
+
+    # Set the SSL context to not verify certificates
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+
+    # Add the generative AI component
+    yourStory = "Star Wars"  # Hard Coded Currently (Needs to be dynamic)
+
+    botResponse = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Print out a picture that describes this: " + yourStory,
+        temperature=0.5,
+        max_tokens=120,
+        top_p=1.0,
+        frequency_penalty=0.8,
+        presence_penalty=0.0,
+    )
+    botResponse = botResponse["choices"][0]["text"].strip()
+
+    response = openai.Image.create(
+        prompt=botResponse,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+
+    # Save the image locally
+    urllib.request.urlretrieve(image_url, "static/result.png")
+
+    # Now, you can pass the image URL to the HTML template
+    image_url = url_for('static', filename='result.png')
+
+    return render_template('intro_flow_3.html', user=current_user, franchise=franchise, image_url=image_url)
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
